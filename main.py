33fb152abe9be4e9545294f6525a55d8612f2673
Ankush <ankushkumar20024@gmail.com>
2025-10-22 22:10:04 +0530

    update

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# print("Django Practice")
-import psycopg2 as pg
-conn = pg.connect(
-    host = "localhost",
-    user = "postgres",
-    password = "1234",
-    dbname = "school"
-)
+# # print("Django Practice")
+# import psycopg2 as pg
+# conn = pg.connect(
+#     host = "localhost",
+#     user = "postgres",
+#     password = "1234",
+#     dbname = "school"
+# )
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM student_details")
-for data in cursor.fetchall():
-    print(data)
-conn.close()
+# cursor = conn.cursor()
+# cursor.execute("SELECT * FROM student_details")
+# for data in cursor.fetchall():
+#     print(data)
+# conn.close()
+
+
+print("Hello World")
