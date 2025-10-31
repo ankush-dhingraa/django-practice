44663563edbdb7dc5b011c28e2e9cc17dd4abba3
Ankush <ankushkumar20024@gmail.com>
2025-10-31 22:56:44 +0530

    creating a model namely task

--- Tutorial and ToDo/ToDoManager/todolist/models.py
+++ Tutorial and ToDo/ToDoManager/todolist/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#creating a model like table structure
+class task(models.Model):
+    task = models.CharField(max_length=200)
+    is_completed = models.BooleanField()
