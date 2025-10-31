06756d91ee5d19fbb91e2239b78f99f4981de2e4
Ankush <ankushkumar20024@gmail.com>
2025-10-31 22:48:29 +0530

    mysql configration

--- Tutorial and ToDo/ToDoManager/ToDoManager/settings.py
+++ Tutorial and ToDo/ToDoManager/ToDoManager/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'ToDoManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'user',
+        'PASSWORD': '1234@5678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
--- Tutorial and ToDo/notes.md
+++ Tutorial and ToDo/notes.md
@@ -8,4 +8,10 @@ python3 manage.py startapp <app name>
 
     **add change in settings.py installed apps name write "app name "
 
+## creating a super user for admin access
+python3 manage.py createsuperuser
+
+## how many packages installed and its versions
+pip freeze
+
 ##
